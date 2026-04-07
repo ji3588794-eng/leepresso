58abaf0aebc3c4ebb0d585d3eeb12a77c69e0183
gihong <devhong97@gmail.com>
2026-04-07 10:30:15 +0900

    20260407 2

--- backend/db.js
+++ backend/db.js
@@ -12,12 +12,12 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   // SSL 설정: DB_SSL이 'true' 문자열일 때만 활성화 (TiDB 등 클라우드 DB용)
-  /* ssl: process.env.DB_SSL === 'true' 
+  ssl: process.env.DB_SSL === 'true' 
     ? {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: false
       }
-    : undefined */
+    : undefined
 });
 
 module.exports = pool;
